23a2f0d5699755f6d56b7264d460c3ba66eced2e
Momo Kornher <mail@moritzkornher.de>
2022-10-16 18:02:55 +0100

    fix: make empty props optional

--- test/backend.integ.ts
+++ test/backend.integ.ts
@@ -1,7 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnOutput } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
 import { Streamlink } from '../src';
 
 const app = new cdk.App();
@@ -15,10 +13,10 @@ const backend = new Streamlink(stack, 'Backend', {});
 
 const endpoint = new lambda.FunctionUrl(stack, 'Endpoint', {
   function: backend.function,
-  authType: FunctionUrlAuthType.NONE,
+  authType: lambda.FunctionUrlAuthType.NONE,
 });
 
-new CfnOutput(stack, 'EndpointUrl', {
+new cdk.CfnOutput(stack, 'EndpointUrl', {
   value: endpoint.url,
 });
 
